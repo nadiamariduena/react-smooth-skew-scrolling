fee4b6083330235a0381327acb43c2c0340e6a80
melissa <nadia.mm.mariduena@gmail.com>
2020-10-11 19:07:52 +0200

    Updates

---
+++ precache-manifest.f4790197f444928960fa43ba6474829d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "858db7a8a4a46399b4b9e0235cab6f9a",
+    "url": "/react-smooth-skew-scrolling/index.html"
+  },
+  {
+    "revision": "14a315b0696f210835b3",
+    "url": "/react-smooth-skew-scrolling/static/css/main.0a039eeb.chunk.css"
+  },
+  {
+    "revision": "c401debc521231c42ae7",
+    "url": "/react-smooth-skew-scrolling/static/js/2.03d1e96a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-smooth-skew-scrolling/static/js/2.03d1e96a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a315b0696f210835b3",
+    "url": "/react-smooth-skew-scrolling/static/js/main.222fabb5.chunk.js"
+  },
+  {
+    "revision": "bf11ec9c3a9344bbbbf0",
+    "url": "/react-smooth-skew-scrolling/static/js/runtime-main.be446fd5.js"
+  }
+]);
