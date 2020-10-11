6c3e872d694dd00b9acf75ae93260f775e5d054a
melissa <nadia.mm.mariduena@gmail.com>
2020-10-11 19:02:28 +0200

    skew ready

--- README.md
+++ README.md
@@ -16,7 +16,7 @@
 
 <ul>
 
-<li>getBoundingClientRect()</li>
+<li>getBoundingClientRect()  this is a method</li>
 
 </ul>
 
@@ -33,7 +33,8 @@ import React, { useRef, useEffect } from "react";
 
 import images from "./images/images";
 function App() {
-  // the useRef must to be added to the respective className , in order to reach these className and make the effect work
+  /*the useRef must to be added to the respective className ,
+  in order to reach these className and make the effect work*/
 
   const app = useRef();
   const scrollContainer = useRef();
@@ -45,8 +46,11 @@ whatever the height of the scrollContainer
 is going to be, FOR THAT you are going to need a useEffect
 
 useEffect?
-Its is a hook that will execute certain FUNCTIONS or commands
-depending of what you want to do, like for example "ANYTIME THE PAGE IS LOADED" or based in certain outcomes or anytime  something CHANGES this effect will run
+Its is a hook that will execute certain FUNCTIONS or
+ commands
+depending of what you want to do, like for example
+"ANYTIME THE PAGE IS LOADED" or based in certain
+outcomes or anytime  something CHANGES this effect will run
 
 */
  <div ref={app} className="App">
@@ -57,9 +61,46 @@ depending of what you want to do, like for example "ANYTIME THE PAGE IS LOADED" 
 
 <br>
 
+#### 2) \_\_ add the following to block the scrolling
+
+```javascript
+.App {
+  /*
+ smooth scrolling TIPS / WARNING!!
+
+you have to apply the following to the children div's in this case the .scroll
+
+ If you want to use this effect in already existent projects where you already have a fixed height
+ , you will have to delete that height and replace it for a padding that will help the content to
+ expand depending of what you have, this padding will give the content a certain amount of space in case
+ the content isn't sufficient.
+  */
+
+  // -----------
+  // this set up here is so that it dont move, at this stage if you check the browser you cannot longer scroll
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  // -----------
+
+  .scroll {
+    padding: 2vh 0;
+    pointer-events: none;
+  }
+```
+
 #### UseEffect :
 
 ###### Its is a hook that will execute certain functions or commands depending of what you want to do, like for example "anytime the page is loaded" or based in certain outcomes or anytime something CHANGES this effect will run.
+
+<br>
+
+#### CPU related comments
+
+##### using the translate3d is a good idea when you want to leverage the CPU since it's using GPU driven animation
 
 <br>
 <br>
@@ -85,4 +126,6 @@ npm i gh-pages --save-dev
 npm i framer-motion
 ```
 
-##### Tutorial by: Wrong Akram
+##### Tutorial by: Wrong Akram , Math data by Jesper Landberg
+
+<a herf="https://jesperlandberg.dev/" target="_blank">website </a>
--- src/App.js
+++ src/App.js
@@ -1,22 +1,84 @@
 import React, { useRef, useEffect } from "react";
-
+import useWindowSize from "./hooks/useWindowSize"; //3
 import images from "./images/images";
 //
 //
 
 function App() {
-  // the useRef must to be added to the respective className , in order to reach these className and make the effect work
+  // HOOK
+  //   4
+  const size = useWindowSize();
+  //
+  //
+  //1  ---------------
+  //   REF
   const app = useRef();
   const scrollContainer = useRef();
+  //
+  //
+  //
+  //
+  //   ---------------
+  // 7 related to the 6 step
+  const skewConfigs = {
+    ease: 0.1, //this is the reason why it dodnt work the first time, the problem was that i set it up at 1 instead of 0.1
+    current: 0,
+    previous: 0,
+    rounded: 0,
+  };
+  //
+  //
+  // 2 ------------------
+  //
+  useEffect(() => {
+    //   what the useEffect is doing here is replacing the componentdidMOunt
+    document.body.style.height = `${
+      scrollContainer.current.getBoundingClientRect().height
+    }px`;
+    // 5  after you add this: const size = useWindowSize(); THEN ADD this: [size.height]);
+  }, [size.height]);
+  //
+  //
+  //
+  // 11 ------------
+  useEffect(() => {
+    requestAnimationFrame(() => skewScrolling());
+  }, []);
+  //
+  //
+  //
+  // 6 ------------------
+  //
+  const skewScrolling = () => {
+    //   all the following is connected to the skewConfigs function on line 18
+    skewConfigs.current = window.scrollY;
+    skewConfigs.previous +=
+      (skewConfigs.current - skewConfigs.previous) * skewConfigs.ease;
+    skewConfigs.rounded = Math.round(skewConfigs.previous * 100) / 100;
 
-  /*
-  The first step is to prevent the scrolling from the app , for that you have to add some additionla scss styles
-  so that the page DONT SCROLL while you work on the effect
-  
-  */
+    //
+    //
+    //
+    // 8 ------------  Difference between
+    const difference = skewConfigs.current - skewConfigs.rounded;
+    const acceleration = difference / size.width; //you access size.width through useWindowSize(); on line 9, thats connected to the function in the HOOK file
+    const velocity = +acceleration;
+    const skew = velocity * 5.5; //original 7.5  the more the scroll is emphasized the more the deg of the image/content , is going to move in a direction
+    //
+    //
+    // 9 ----------   ASSIGN the scroll container the skew
+    scrollContainer.current.style.transform = `translate3d(0, -${skewConfigs.rounded}px, 0) skewY(${skew}deg)`;
+    //
+    //
+    // 10 ------------
+
+    requestAnimationFrame(() => skewScrolling());
+  };
+  //
   //
 
   return (
+    //   2  ref={app} and ref={scrollContainer}
     <div ref={app} className="App">
       <div ref={scrollContainer} className="scroll">
         {images.map((image, index) => (
@@ -25,7 +87,7 @@ function App() {
               <img src={image} alt={`people ${index}`} />
             </div>
             <h2>
-              Skewww <span className="outline"> Scrolllingg</span>
+              Skewww <span className="outline"> Scroll</span>
             </h2>
           </>
         ))}
@@ -34,3 +96,35 @@ function App() {
   );
 }
 export default App;
+
+/*
+
+  const skewScrolling = () => {
+    skewConfigs.current = window.scrollY;
+    skewConfigs.previous +=
+      (skewConfigs.current - skewConfigs.previous) * skewConfigs.ease;
+    skewConfigs.rounded = Math.round(skewConfigs.previous * 100) / 100;
+
+    // variables for the velocity
+    //
+    // Difference between
+    const difference = skewConfigs.current - skewConfigs.rounded;
+    const acceleration = difference / size.width;
+    const velocity = +acceleration;
+    const skew = velocity * 7.5;
+
+    //
+    //
+    //
+    scrollContainer.current.style.transform = `translate3d(0, -${skewConfigs.rounded}px, 0) skewY(${skew}deg)`;
+    //
+    //
+    //
+
+    requestAnimationFrame(() => skewScrolling());
+
+
+
+
+
+*/
